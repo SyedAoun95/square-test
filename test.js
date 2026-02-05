3fe6c983fc628a77e69c63720614a90c964c0012
Syedaoun95 <aouns6229@gmail.com>
2026-02-05 15:21:41 +0500

    ecternal link warning script

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
 console.log("Script loaded from GitHub repo");
+// Email and phone number labels
+  document.addEventListener('DOMContentLoaded', () => {
+    const handleContactLinks = () => {
+      const links = document.querySelectorAll('a[href^="mailto:"], a[href^="tel:"]');
+  
+      links.forEach(link => {
+        const href = link.getAttribute('href');
+        let linkText = link.textContent.trim();
+  
+        if (href.startsWith('mailto:')) {
+          link.setAttribute('aria-label', `Email Address: ${linkText}`);
+        } else if (href.startsWith('tel:')) {
+          // remove tel
+          linkText = linkText.replace(/^(Tel:|Phone:)\s*/i, '');
+          link.setAttribute('aria-label', `Phone number: ${linkText}`);
+        }
+      });
+    };
+  
+    handleContactLinks();
+    console.log("warnings added")
+  });
